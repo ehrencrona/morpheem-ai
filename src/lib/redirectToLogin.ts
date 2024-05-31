196ddbb178eb7c65c5874a07f2f710dcc8ef5eff
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-05-31 17:09:06 +0900

    redirect to login was broken

--- src/lib/redirectToLogin.ts
+++ src/lib/redirectToLogin.ts
@@ -1,10 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export function redirectToLogin(url: URL) {
-	return {
-		status: 302,
-		headers: {
-			location: redirect(302, `/login?return_to=${encodeURIComponent(url.pathname)}`)
-		}
-	};
+	return redirect(302, `/login?return_to=${encodeURIComponent(url.pathname)}`);
 }
