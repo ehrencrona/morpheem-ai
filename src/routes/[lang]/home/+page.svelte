937ff5ce286670b19ed9abbdbec9a54181ac6511
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-06-04 19:03:38 +0900

    progress chart

--- src/routes/[lang]/home/+page.svelte
+++ src/routes/[lang]/home/+page.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
-	import {
-		getPastDue,
-		getRepetitionTime,
-		setPastDue,
-		setRepetitionTime
-	} from '$lib/settings';
+	import { getPastDue, getRepetitionTime, setPastDue, setRepetitionTime } from '$lib/settings';
 	import { onMount } from 'svelte';
 	import { getLanguageOnClient } from '../api/api-call';
 	import type { PageData } from './$types';
 	import RecentSentences from './RecentSentences.svelte';
 	import RecentWords from './RecentWords.svelte';
+	import Plot from 'svelte-plotly.js';
 
 	export let data: PageData;
 
@@ -29,6 +25,40 @@
 		repetitionTime = getRepetitionTime();
 		pastDue = getPastDue();
 	});
+
+	const readData = data.activity
+		.map(({ words_known }, i) => ({ day: data.activity.length - 1 - i, y: words_known }))
+		.filter(({ y }) => y > 0);
+
+	let readTrace = {
+		x: readData.map(({ day }) => day),
+		y: readData.map(({ y }) => y),
+		type: 'scatter',
+	};
+
+	const writeData = data.activity
+		.map(({ words_known_write }, i) => ({
+			day: data.activity.length - 1 - i,
+			y: words_known_write
+		}))
+		.filter(({ y }) => y > 0);
+
+	let writeTrace = {
+		x: writeData.map(({ day }) => day),
+		y: writeData.map(({ y }) => y),
+		type: 'scatter',
+	};
+
+	const timeData = data.activity
+		.map(({ minutes_spent }, i) => ({ day: data.activity.length - 1 - i, y: minutes_spent }))
+		.filter(({ y }) => !!y);
+
+	let timeTrace = {
+		x: timeData.map(({ day }) => day),
+		y: timeData.map(({ y }) => y),
+		type: 'scatter',
+		name: 'Time spent'
+	};
 </script>
 
 <a
@@ -92,6 +122,45 @@
 
 <h3 class="text-lg mb-4 mt-8">Your progress</h3>
 
+<div class="flex flex-wrap mb-8">
+	{#if readTrace.x.length > 2 || writeTrace.x.length > 2}
+		<div class="h-[200px] min-w-[300px] flex-1">
+			<div class="text-center text-xs font-lato">Vocabulary</div>
+			<Plot
+				data={[readTrace, writeTrace]}
+				config={{ displayModeBar: false }}
+				layout={{
+					margin: { t: 0, l: 40, b: 20, r: 10 },
+					font: {
+						size: 10
+					},
+					showlegend: false
+				}}
+				fillParent={true}
+				debounce={100}
+			/>
+		</div>
+	{/if}
+
+	{#if timeTrace.x.length > 2}
+		<div class="h-[200px] min-w-[300px] flex-1">
+			<div class="text-center text-xs font-lato">Minutes per day</div>
+			<Plot
+				data={[timeTrace]}
+				config={{ displayModeBar: false }}
+				layout={{
+					margin: { t: 0, l: 40, b: 20, r: 10 },
+					font: {
+						size: 10
+					}
+				}}
+				fillParent={true}
+				debounce={100}
+			/>
+		</div>
+	{/if}
+</div>
+
 <div class="grid grid-cols-5 gap-h-1 max-w-[500px] text-xs">
 	<div class="col-span-3"></div>
 	<div class="col-span-2 text-right">Vocabulary size</div>
@@ -100,7 +169,7 @@
 	<div class="mb-2 text-right">Sentences</div>
 	<div class="mb-2 text-right">Passive</div>
 	<div class="mb-2 text-right">Active</div>
-	{#each data.activity as date}
+	{#each data.activity.slice(0, 10) as date}
 		<div class="mb-1">
 			{dateFormat.format(date.date)}
 		</div>
