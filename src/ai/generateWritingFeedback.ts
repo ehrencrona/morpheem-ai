fc35600f8d44d575f066cd7135b4dfc01c0b4993
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-06-07 09:14:06 +0900

    tweaked writing prompt

--- src/ai/generateWritingFeedback.ts
+++ src/ai/generateWritingFeedback.ts
@@ -30,7 +30,7 @@ export async function generateWritingFeedback(
 					`Briefly but friendly point out any grammatical mistakes, spelling mistakes or unidiomatic constructs. If the word is missing, say so. ` +
 					`If the sentence is correct, praise the user. Also return a corrected text, applying all your suggestions. ` +
 					`Return a boolean indicating whether the sentence was grammatically correct (ignoring punctuation and minor typos). ` +
-					`If there is only a single word that is incorrect return it (as it is written in the corrected sentence). ` +
+					`If there is only a single word that is incorrect return it (as it is written in the corrected sentence); if there are multiple words do not return a word. ` +
 					`Write in English. Return JSON in the format {feedback: string, corrected: string, isCorrect: boolean, word?: string}`
 			},
 			{
@@ -63,7 +63,7 @@ export async function generateTranslationFeedback(
 					`Briefly but friendly point out any grammatical mistakes, spelling mistakes, unidiomatic constructs or divergences in meaning. ` +
 					`If the sentence generally captures the intended meaning and is grammatically correct (it does not need to match the provided translation), praise the user. ` +
 					`Return a boolean indicating whether it is a correct translation (ignoring punctuation and minor typos). ` +
-					`If there is only a single word that is incorrect return it (as it is written in the correct translation). ` +
+					`If there is only a single word that is incorrect return it (as it is written in the corrected sentence); if there are multiple words do not return a word. ` +
 					`Write in English. Return JSON in the format {feedback: string, isCorrect:boolean, word?:string}`
 			},
 			{
