078c8f481ab9e75d2df8a6cf497c36405512306c
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-06-16 13:47:22 +0400

    language selector working on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,9 @@
-
 <div class="w-full mt-60 flex justify-center items-center">
-  <div class="flex gap-20">
-    <a href="/fr" class="bg-blue-3 text-blue-1 py-2 px-8 text-xl"> French </a>
+	<div class="flex flex-wrap gap-20">
+		<a href="/fr" class="bg-blue-3 text-blue-1 py-2 px-8 text-xl"> French </a>
 
-    <a href="/es" class="bg-blue-3 text-blue-1 py-2 px-8 text-xl"> Spanish </a>
-  
-    <a href="/pl" class="bg-blue-3 text-blue-1 py-2 px-8 text-xl"> Polish </a>
-  </div>
+		<a href="/es" class="bg-blue-3 text-blue-1 py-2 px-8 text-xl"> Spanish </a>
+
+		<a href="/pl" class="bg-blue-3 text-blue-1 py-2 px-8 text-xl"> Polish </a>
+	</div>
 </div>
-
