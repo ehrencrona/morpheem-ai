0739800443f7177815df3724b850b9c8fef35d88
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-06-16 08:40:23 +0400

    trying to make cloze evaluation stricter

--- src/ai/evaluateCloze.ts
+++ src/ai/evaluateCloze.ts
@@ -29,7 +29,7 @@ export async function evaluateCloze(
 				role: 'system',
 				content:
 					`The user is solving the following ${language.name} cloze exercise:\n\n${cloze}\n\n` +
-					`We were looking for the right answer "${correctAnswer}". The clue "${clue}" was provided.\n` +
+					`We are looking for the right answer "${correctAnswer}". The clue "${clue}" was provided.\n` +
 					(isWrongInflection
 						? `Very briefly explain which grammatical form of the word is required and why. Also explain what form the user picked. Address the user as "you."\n`
 						: `If the user's answer is similar to the right answer, very briefly explain their relationship in terms of meaning or grammar.\n` +
@@ -37,7 +37,7 @@ export async function evaluateCloze(
 							`If the user chose an answer that is possible in the cloze, finish with "${POSSIBLE_ANSWER_STRING}"\n`) +
 					`Answer nothing else:\n`
 			},
-			{ role: 'user', content: userAnswer }
+			{ role: 'user', content: cloze.replace(/_+/, `***` + userAnswer + `***`) }
 		],
 		model: 'gpt-4o',
 		temperature: 0.5,
