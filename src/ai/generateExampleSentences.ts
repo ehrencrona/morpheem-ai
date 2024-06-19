86643633e96af08a5a40ddf4b16d5ef723df8aae
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-06-19 16:34:32 +0400

    ask for more diversity in prompts

--- src/ai/generateExampleSentences.ts
+++ src/ai/generateExampleSentences.ts
@@ -21,7 +21,9 @@ export async function generateExampleSentences(
 			instruction: `Return JSON in the format { "examples": [ ... ]}. Do not translate.`,
 			prompt:
 				`Give ${count} ${language.name} sentences illustrating the use of the word "${lemma}". If the word is not a ${language.name} word, do not return anything. ` +
-				`The sentences are intended for adults. If you need names of people, use ${names[language.code]}.` +
+				`The sentences are intended for adults. ` +
+				`Use different styles such news text, fragments of dialog (e.g. in a shop, a workplace or a phone call, do not add the name of the speaker), informal language, advertising language, bureaucratic language, questions etc. ` +
+				`If you need names of people, use ${names[language.code]}.` +
 				(level != 'normal'
 					? level == 'beginner'
 						? ` Use only very simple words.`
