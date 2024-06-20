e21170bb7acdf812eedbae12137c5d5f7e9bb4bc
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-06-20 16:29:09 +0400

    update word types

--- src/scripts/updateWordTypes.ts
+++ src/scripts/updateWordTypes.ts
@@ -1,45 +1,59 @@
+import { parallelize } from '$lib/parallelize';
 import { classifyLemmas } from '../ai/classifyLemmas';
 import { KOREAN, POLISH } from '../constants';
 import { db } from '../db/client';
 import { getWords } from '../db/words';
+import * as DB from '../db/types';
 
 const language = KOREAN;
 
 async function updateWordTypes(run: 'dry' | 'real') {
 	const words = await getWords('id asc', language);
 
-	for (let i = 0; i < words.length; i += 30) {
-		const wordsToCheck = words.slice(i, i + 30).map(({ word }) => word);
+	const slices: string[][] = [];
 
-		const cognates = await classifyLemmas(wordsToCheck, { language, throwOnInvalid: false });
+	for (let i = 0; i < words.length; i += 25) {
+		slices.push(words.slice(i, i + 25).map(({ word }) => word));
+	}
 
-		for (const word of wordsToCheck) {
-			const type = cognates.find(({ lemma }) => lemma === word)?.type || null;
+	await parallelize(
+		slices.map((wordsToCheck) => async () => {
+			const cognates = await classifyLemmas(wordsToCheck, { language, throwOnInvalid: false });
 
-			if (type == 'wrong' || type == 'inflection') {
-				console.warn(
-					`Invalid lemma: ${word} (${type}).` + (run == 'dry' ? '' : ` Deleting word...`)
-				);
+			for (const word of wordsToCheck) {
+				const type = cognates.find(({ lemma }) => lemma === word)?.type || null;
 
-				if (run == 'real') {
-					await db.withSchema(language.code).deleteFrom('words').where('word', '=', word).execute();
-				}
-			} else {
-				if (type == 'name' || type == 'particle' || type == 'cognate') {
-					console.log(`Updating word: ${word} (${type})`);
-				}
+				if (type == 'wrong' || type == 'inflection') {
+					console.warn(
+						`Invalid lemma: ${word} (${type}).` + (run == 'dry' ? '' : ` Deleting word...`)
+					);
 
-				if (run == 'real') {
-					await db
-						.withSchema(language.code)
-						.updateTable('words')
-						.set({ type })
-						.where('word', '=', word)
-						.execute();
+					if (run == 'real') {
+						await db
+							.withSchema(language.code)
+							.deleteFrom('words')
+							.where('word', '=', word)
+							.execute();
+					}
+				} else {
+					if (type == 'name' || type == 'particle' || type == 'cognate') {
+						console.log(`Updating word: ${word} (${type})`);
+					}
+
+					if (run == 'real') {
+						await db
+							.withSchema(language.code)
+							.updateTable('words')
+							.set({ type })
+							.where('word', '=', word)
+							.execute();
+					}
 				}
 			}
-		}
-	}
+		}),
+		4,
+		true
+	);
 }
 
-updateWordTypes('dry');
+updateWordTypes('real');
