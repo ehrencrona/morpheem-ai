15fa7c169881134f773837b3bf721ea9a001d1c6
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-07-01 08:19:11 +0400

    nicer offline error

--- src/components/ErrorMessage.svelte
+++ src/components/ErrorMessage.svelte
@@ -5,6 +5,18 @@
 	const onClear = () => {
 		errorStore.set(null);
 	};
+
+	function toMessage(error: any) {
+		if (error instanceof Error) {
+			if (error.message == 'Failed to fetch') {
+				return 'You seem to be offline.';
+			}
+
+			return error.message;
+		}
+
+		return error.toString();
+	}
 </script>
 
 {#if $errorStore}
@@ -12,7 +24,7 @@
 		class="flex fixed left-0 bottom-0 right-0 p-2 bg-red text-[#fff] text-xs font-lato z-50"
 		transition:fade
 	>
-		<div class="flex-1">{$errorStore.message || $errorStore.toString()}</div>
+		<div class="flex-1">{toMessage($errorStore)}</div>
 		<div>
 			<button class="underline ml-2" on:click={onClear}>Close</button>
 		</div>
