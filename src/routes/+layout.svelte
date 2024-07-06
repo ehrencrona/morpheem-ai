535cbef03dc778995ee8b07f7fb5fc5bc04981bb
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-07-06 14:02:02 +0400

    a bit more space for reader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script>
 	import '../app.css';
 	import Menu from '../components/Menu.svelte';
+	import { page } from '$app/stores';
+
+	$: isReader = $page.url.pathname.includes('/reader');
 </script>
 
 <Menu></Menu>
 
 <div class="flex align-center justify-center p-4 pb-[170px]">
-	
-	<div class="w-full lg:w-[800px]">
+	<div class="w-full {isReader ? `lg:w-[900px]` : `lg:w-[800px]`}">
 		<slot />
 	</div>
 </div>
