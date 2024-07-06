f3427f52a71ba29733fa45bc08011674ce9a0b6a
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-07-06 14:24:30 +0400

    do not count corrected words as known

--- src/logic/storeWrittenSentence.ts
+++ src/logic/storeWrittenSentence.ts
@@ -3,6 +3,7 @@ import { getMultipleWordsByLemmas } from '../db/words';
 import { addWrittenSentence } from '../db/writtenSentences';
 import { addSentence } from './addSentence';
 import { lemmatizeSentences } from './lemmatize';
+import { toWordStrings } from './toWordStrings';
 import { Language } from './types';
 
 export async function storeWrittenSentence({
@@ -18,6 +19,7 @@ export async function storeWrittenSentence({
 	userId: number;
 	language: Language;
 }) {
+	const wordStrings = toWordStrings(sentenceString, language);
 	const [lemmatized] = await lemmatizeSentences([sentenceString], { language });
 
 	const [sentence, words] = await Promise.all([
@@ -39,14 +41,40 @@ export async function storeWrittenSentence({
 		})
 	]);
 
-	const knowledge = words.map((word) => ({
-		word,
-		wordId: word.id,
-		isKnown: true,
-		sentenceId: sentence.id,
-		type: KNOWLEDGE_TYPE_WRITE,
-		userId
-	}));
+	let nonEntered: string[] = [];
+
+	const knowledge = words
+		.filter((word) => {
+			const index = lemmatized.indexOf(word.word);
+
+			let conjugated = wordStrings[index];
+
+			if (conjugated == null) {
+				console.warn(`Could not find word ${word.word} in ${sentenceString}`);
+
+				conjugated = word.word;
+			}
+
+			const wasEntered = entered.includes(conjugated);
+
+			if (!wasEntered) {
+				nonEntered.push(conjugated);
+			}
+
+			return wasEntered;
+		})
+		.map((word) => ({
+			word,
+			wordId: word.id,
+			isKnown: true,
+			sentenceId: sentence.id,
+			type: KNOWLEDGE_TYPE_WRITE,
+			userId
+		}));
+
+	console.log(
+		`The following words were in the correction but not in the original sentence: ${nonEntered.join(', ')}`
+	);
 
 	return { sentence, lemmatized, knowledge };
 }
