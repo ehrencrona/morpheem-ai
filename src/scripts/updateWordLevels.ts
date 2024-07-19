36476dc57af649e4fe2236746c36fabd9cec80db
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-07-19 18:29:53 +0400

    faster updateWordLevels

--- src/scripts/updateWordLevels.ts
+++ src/scripts/updateWordLevels.ts
@@ -1,34 +1,52 @@
 import { sql } from 'kysely';
 import { db } from '../db/client';
+import { getUnits } from '../db/units';
+import { POLISH } from '../constants';
+import { parallelize } from '$lib/parallelize';
+
+const LEVELS_FOR_UNITS = 15;
+const MAX_LEVEL = 100;
 
 async function updateWordLevels() {
+	const units = await getUnits(POLISH);
+
 	const frequencies = await sql<{
 		word: string;
+		unit: number | null;
 		word_id: number;
 		frequency: number;
 	}>`
-    SELECT word_id, word, COUNT(*) AS frequency
+    SELECT word_id, unit, word, COUNT(*) AS frequency
       FROM pl.word_sentences
       JOIN pl.words ON word_id = id
-      GROUP BY word_id, word
+      GROUP BY word_id, unit, word
       ORDER BY frequency DESC`.execute(db);
 
 	const count = frequencies.rows.length;
 
-	let i = 0;
+	await parallelize(
+		frequencies.rows.map(({ word, unit, word_id }, i) => async () => {
+			let level = Math.round(MAX_LEVEL * Math.sqrt(i++ / count));
 
-	for (const { word, word_id } of frequencies.rows) {
-		const level = Math.round(100 * Math.sqrt(i++ / count));
+			if (units.length) {
+				if (unit != null) {
+					level = Math.round((unit / units.length) * LEVELS_FOR_UNITS);
+				} else {
+					level = LEVELS_FOR_UNITS + Math.round(level * (1 - LEVELS_FOR_UNITS / MAX_LEVEL));
+				}
+			}
 
-		console.log(`${word}\t${level}`);
+			console.log(`${word}\t${unit || '-'}\t${level}`);
 
-		await db
-			.withSchema('pl')
-			.updateTable('words')
-			.set({ level })
-			.where('id', '=', word_id)
-			.execute();
-	}
+			await db
+				.withSchema('pl')
+				.updateTable('words')
+				.set({ level })
+				.where('id', '=', word_id)
+				.execute();
+		}),
+		10
+	);
 
 	process.exit(0);
 }
