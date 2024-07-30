264d77f5d6995002d8cf20579f3a972540bb62c3
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-07-30 10:29:21 +0400

    prevent race conditions in unknown lookup

--- src/routes/[lang]/api/word/unknown/client.ts
+++ src/routes/[lang]/api/word/unknown/client.ts
@@ -1,12 +1,25 @@
 import { apiCall } from '../../api-call';
 import type { PostSchema, UnknownWordResponse } from './+server';
 
+let cache: Record<string, Promise<UnknownWordResponse>> = {};
+
 export async function lookupUnknownWord(
 	word: string,
 	{ sentenceId, sentence }: { sentenceId?: number; sentence?: string } = {}
 ): Promise<UnknownWordResponse> {
-	return apiCall(`/api/word/unknown`, {
-		method: 'POST',
-		body: JSON.stringify({ word, sentenceId, sentence } satisfies PostSchema)
-	});
+	const key = `${word}-${sentenceId}-${sentence}`;
+
+	let result: Promise<UnknownWordResponse> = cache[key];
+
+	if (result == undefined) {
+		result = cache[key] = apiCall(`/api/word/unknown`, {
+			method: 'POST',
+			body: JSON.stringify({ word, sentenceId, sentence } satisfies PostSchema)
+		}).catch((e) => {
+			delete cache[key];
+			throw e;
+		});
+	}
+
+	return result;
 }
