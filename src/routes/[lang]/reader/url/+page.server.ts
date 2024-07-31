ab7a140e4e1c0cdefe842198af3c500b5a5e49f9
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-07-31 10:32:59 +0400

    error handling reader

--- src/routes/[lang]/reader/url/+page.server.ts
+++ src/routes/[lang]/reader/url/+page.server.ts
@@ -1,7 +1,8 @@
-import { extract } from '@extractus/article-extractor';
+import { ArticleData, extract } from '@extractus/article-extractor';
 import { ServerLoad, error } from '@sveltejs/kit';
 import * as htmlparser2 from 'htmlparser2';
 import { Paragraph } from './Paragraph';
+import { logError } from '$lib/logError';
 
 export const load: ServerLoad = async ({ locals: { language, userId }, url }) => {
 	const articleUrl = url.searchParams.get('url');
@@ -10,71 +11,112 @@ export const load: ServerLoad = async ({ locals: { language, userId }, url }) =>
 		return error(400, 'Bad Request');
 	}
 
-	console.log(`Reading article ${articleUrl} (user ${userId})...`);
+	let title: string | undefined;
+	let pages: Paragraph[][] = [];
 
 	const atPage = (parseInt(url.searchParams.get('page') || `1`) || 1) - 1;
 
-	const article = await extract(articleUrl);
+	try {
+		console.log(`Reading article ${articleUrl} (user ${userId})...`);
 
-	if (!article) {
-		return error(404, 'Failed to read article');
-	}
+		const article = await extract(articleUrl);
 
-	let pages: Paragraph[][] = [];
+		if (article) {
+			title = article.title;
 
-	let currentPage: Paragraph[] = [];
-	let currentParagraph = '';
-	let style: 'h' | 'p' = 'p';
+			let currentPage: Paragraph[] = [];
+			let currentParagraph = '';
+			let style: 'h' | 'p' = 'p';
 
-	function pageEnds() {
-		if (currentPage.length) {
-			pages.push(currentPage);
-			currentPage = [];
-		}
-	}
-
-	function paragraphEnds() {
-		if (currentParagraph.trim()) {
-			if (currentPage.reduce((acc, { text }) => acc + text.length, 0) > 120) {
-				pageEnds();
+			function pageEnds() {
+				if (currentPage.length) {
+					pages.push(currentPage);
+					currentPage = [];
+				}
 			}
 
-			currentPage.push({
-				text: currentParagraph.trim(),
-				style
+			function paragraphEnds() {
+				if (currentParagraph.trim()) {
+					if (currentPage.reduce((acc, { text }) => acc + text.length, 0) > 120) {
+						pageEnds();
+					}
+
+					currentPage.push({
+						text: currentParagraph.trim(),
+						style
+					});
+				}
+
+				currentParagraph = '';
+				style = 'p';
+			}
+
+			const parser = new htmlparser2.Parser({
+				onopentag(name, attributes) {
+					if (name[0] == 'h' && name.length == 2) {
+						style = 'h';
+						pageEnds();
+					}
+				},
+				ontext(text) {
+					currentParagraph += ' ' + text;
+				},
+
+				onclosetag(tagname) {
+					if (tagname === 'p' || tagname == 'div' || (tagname[0] == 'h' && tagname.length == 2)) {
+						paragraphEnds();
+					}
+				}
 			});
-		}
 
-		currentParagraph = '';
-		style = 'p';
+			paragraphEnds();
+			pageEnds();
+
+			parser.write(article!.content || '');
+		} else {
+			logError(`Unable to read article ${articleUrl} (user ${userId})`);
+
+			title = 'Unable to read article';
+
+			pages = [
+				[
+					{
+						text: 'Sorry, we were unable to read this article. This is probably due to technical details relating to how the site of the article is built.',
+						style: 'p'
+					},
+					{
+						text: 'Please try another article or another site.',
+						style: 'p'
+					}
+				]
+			];
+		}
+	} catch (e) {
+		logError(e, `While reading article ${articleUrl} (user ${userId})`);
+
+		title = 'Unable to read article';
+
+		pages = [
+			[
+				{
+					text: 'Sorry, we were unable to read this article. This is probably due to technical issues relating to how the site of the article is built.',
+					style: 'p'
+				},
+				{
+					text: 'Please try another article or another site.',
+					style: 'p'
+				},
+				{
+					text: `Error message: "${(e as any).message}"`,
+					style: 'p'
+				}
+			]
+		];
 	}
-
-	const parser = new htmlparser2.Parser({
-		onopentag(name, attributes) {
-			if (name[0] == 'h' && name.length == 2) {
-				style = 'h';
-				pageEnds();
-			}
-		},
-		ontext(text) {
-			currentParagraph += ' ' + text;
-		},
-
-		onclosetag(tagname) {
-			if (tagname === 'p' || tagname == 'div' || (tagname[0] == 'h' && tagname.length == 2)) {
-				paragraphEnds();
-			}
-		}
-	});
-
-	paragraphEnds();
-	pageEnds();
-
-	parser.write(article!.content || '');
 
 	return {
 		articleUrl,
-		title: article.title,
+		title,
 		language,
 		pages,
 		atPage
