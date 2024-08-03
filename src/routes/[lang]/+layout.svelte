a04784887f656d7854cda663fa6a8d4019c1be70
Andreas Ehrencrona <andreas.ehrencrona@velik.it>
2024-08-03 11:13:35 +0400

    page too wide on mobile on suggestions

--- src/routes/[lang]/+layout.svelte
+++ src/routes/[lang]/+layout.svelte
@@ -10,7 +10,7 @@
 	<Menu></Menu>
 
 	<div class="flex flex-1 align-center justify-center p-4 pb-[170px]">
-		<div class="{isWide ? `` : `w-full lg:w-[800px] lg:max-w-[95vw]`}">
+		<div class="{isWide ? `` : `w-full lg:w-[800px] max-w-[100vw] lg:max-w-[95vw]`}">
 			<slot />
 		</div>
 	</div>
